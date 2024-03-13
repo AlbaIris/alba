489d1ecbf1368b8e3b13ed4e62d43bfb3461c856
AlbaIris <162179042+AlbaIris@users.noreply.github.com>
2024-03-13 14:37:05 -0400

    Add files via upload

--- proyect.py
+++ proyect.py
@@ -17,7 +17,9 @@ def hello_proyect(num1, num2, num3, operaciones):
     if __name__ == ('__main_') :
      app.run(debug=True)
 
-def hello_proyect(num1, num2, operaciones):
+@app.route('/')
+@app.route("/calculadora/num1/num2/operaciones")
+def hello_open(num1, num2, operaciones):
    if operaciones == 'suma':
         return num1+num2 
    if operaciones == 'resta':
